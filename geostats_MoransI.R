7fa7cef44ad2a3ec912b20f77ac0f2fa2c4a7d18
Micha Silver <micha@arava.co.il>
2017-05-03 13:40:10 +0300

    Renamed R scripts

---
+++ geostats_MoransI.R
@@ -0,0 +1,70 @@
+library('spatstat')
+library('dplyr')
+library('sp')
+library('spdep')
+library('dismo')
+
+# ---------------------------------
+# Load data,
+# ---------------------------------
+# Load data and metadata
+setwd(paste0('/home/micha/Studies/Courses',
+             '/Geostatistics-Tal/Final Project/'))
+
+# WHich data interval?
+data_interval <- 'daily'
+
+if (data_interval == 'daily') {
+  date_str <- '20170112'
+  data_file <- paste0('gauge_data/','gauge_data_daily.csv')
+  data_cols <- c('station_id','date_time','quality',
+                 'obs_precip', 'precip_ind','snow','eor')
+} else {
+  date_str <- '2017011201'
+  data_file <- paste0('gauge_data/','gauge_data_hourly.csv')  
+  data_cols <- c('station_id','date_time','quality',
+                'precip_ind', 'obs_precip','form','eor')
+}
+gauge_data <- read.csv(data_file, col.names=data_cols)
+
+meta_cols <- c('station_id','from_date','to_date','elevation',
+               'latitude','longitude',
+               'stn_name','province')
+gauge_metadata <- read.csv('gauge_data/gauge_metadata.csv', col.names=meta_cols)
+#head(gauge_data)
+#head(gauge_metadata)
+
+# Get one day (or hour), and attach metadata
+gauge_data_filtered <- filter(gauge_data, date_time==date_str)
+head(gauge_data_filtered)
+gauges <- merge(gauge_data_filtered, gauge_metadata, by='station_id', all.y=TRUE)
+# Make sure to clean out NA or < 0 (unknown values)
+gauges <- na.omit(gauges)
+gauges <- filter(gauges, obs_precip>=0)
+
+# Weights matrix
+distances <- as.matrix(dist(gauges))
+diag(distances) <- 0
+inv_dist = 1/distances
+inv_dist_squared <- 1/distances^2
+wts <- mat2listw(inv_dist)
+
+# Prepare SPDF
+xy_coords <- cbind(gauges$longitude, gauges$latitude) 
+proj_wgs84 = CRS("+init=epsg:4326")
+coordinates(gauges) <- xy_coords
+proj4string(gauges) <- proj_wgs84
+
+# Read in aspect raster
+aspect_bavaria <- raster('GIS/aspect_bavaria.tif')
+# Extract aspect from raster at gauge locations, 
+# and add values to new data column
+gauges$aspect <- extract(aspect_bavaria, gauges)
+# Now we have three possible predictors: elevation, latitude, aspect
+# dependant variable is obs_precip
+head(gauges$aspect)
+
+# Moran's I
+moran.test(gauges$obs_precip,w)
+# Geary's C
+geary.test(gauges$obs_precip, w, zero.policy = T)
